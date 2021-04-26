dd712b06554537a12ed12fe8c98bf8e108510a26
calise cheung <cacheung@adobe.com>
2021-04-26 13:40:29 -0700

    update swift package Manager's dependency to version based.
    update swift package Manager's dependency to version based.

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
         .library(name: "AEPAnalyticsStatic", type: .static, targets: ["AEPAnalytics"]),
     ],
     dependencies: [
-        .package(name: "AEPCore", url: "https://github.com/adobe/aepsdk-core-ios.git", .branch("main")),
+        .package(url: "https://github.com/adobe/aepsdk-core-ios.git", from: "3.0.0"),
     ],
     targets: [
         .target(name: "AEPAnalytics",
